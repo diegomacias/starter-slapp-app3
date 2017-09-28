610945b43c3ba5d6e0d2a5ffab19627b41faa9ae
Diego Macias Rodriguez <diegof.macias@softtek.com>
2017-09-28 13:04:40 -0500

    Agregamos Fix global

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 'use strict'
 
-
-/*
-
-Parameters
-
-criteria text that message contains or regex (e.g. "^hi")
-typeFilter [optional] Array for multiple values or string for one value. Valid values are direct_message, direct_mention, mention, ambient
-callback function - (msg, text, [match1], [match2]...) => {}
-*/
-
 const express = require('express')
 const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
@@ -96,7 +86,7 @@ slapp
 
 // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
 slapp
-  .message('^(accidentes|choques|accident)$', ['criteria', 'direct_message'], (msg, text) => {
+  .message('^(accidentes|choques|accident)$', ['direct_mention', 'mention'], (msg, text) => {
     
     //msg.say(`${text}, how are you?`) //aqui toma una variable y la adjunta a la respuesta
       msg.say(`Quieres Saber los accidentes que tiene AGS el dia de hoy ?`)
@@ -110,60 +100,33 @@ slapp
     // user may not have typed text as their next action, ask again and re-route
     if (!text) {
       return msg
-        if (msg == "Si" || msg == "No" || msg == "yes" || msg == "no"){
-
-      if (msg == "si") {
-        .say(`los accidentes han sido muy Desastrosos por la concurrencia de lluvia en los ultimos 3 dias en el estado de aguascalientes Quieres hablar de otro tema en especifico ?`)
-        .route('hablar-de-otra-cosa-en-especifico', state)
-         }else(){
-
-
-      }else{
-
-            .say("No te entendi, Disculpa Soy muy torpe \n Selecciona para entender mejor")
-              
-            msg.say({
-            text: '',
-            attachments: [
-              {
-                text: 'Quieres Saber los accidentes que tiene AGS ?',
-                fallback: 'Quieres Saber los accidentes que tiene AGS ? ',
-                callback_id: 'doit_confirm_callback',
-                actions: [
-                  { name: 'answer', text: 'Si', type: 'button', value: 'si' },
-                  { name: 'answer', text: 'No', type: 'button', value: 'no' }
-                ]
-              }]});
-        
-              }
-      }
-
-
         .say("Whoops, I'm still waiting to hear how you're doing.")
         .say('How are you?')
-        .route('respuesta-accidentes', state)
+        .route('how-are-you', state)
     }
 
     // add their response to state
     state.status = text
 
     msg
+      .say(`los accidentes han sido muy Desastrosos por la concurrencia de lluvia en los ultimos 3 dias en el estado de aguascalientes`)
+      .route('color', state)
   })
-  .route('hablar-de-otra-cosa-en-especifico', (msg, state) => {
+  .route('color', (msg, state) => {
     var text = (msg.body.event && msg.body.event.text) || ''
 
     // user may not have typed text as their next action, ask again and re-route
     if (!text) {
       return msg
-        .say("Necesito que me digas Algo")
-        .route('hablar-de-otra-cosa-en-especifico', state)
+        .say("I'm eagerly awaiting to hear your favorite color.")
+        .route('color', state)
     }
 
     // add their response to state
     state.color = text
 
     msg
-      .say('Gracias por platicar Conmigo, Ultimamente me siento solo, esta es nuestra platica:')
+      .say('Thanks for sharing.')
       .say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
     // At this point, since we don't route anywhere, the "conversation" is over
   })
